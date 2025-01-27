c8fce035867626cc9aa8f53f7b4d13c02dc03a19
Шатуева Полина Николаевна <p.shatueva@main.nsu.ru>
2025-01-27 14:14:47 +0700

    k

--- chess.py
+++ chess.py
@@ -3,3 +3,14 @@
 # Укажите координаты клетки, где находится ферзь, например, [4,4].
 # Определите клетки, которые атакует ферзь (в строке, столбце и диагоналях).
 # Визуализация: Используйте тепловую карту (imshow), чтобы показать шахматную доску. Отметьте положение ферзя и атакуемые клетки цветами.
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as clr
+matrix = np.array([[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8,]])
+print(matrix)
+plt.imshow(data, cmap='viridis')
+plt.colorbar(label="Интенсивность")
+plt.title("Тепловая карта")
+plt.show()
+def sdelai_docsku(board):
+    board = np.zeros(shape:(size,size), dtype = int)
